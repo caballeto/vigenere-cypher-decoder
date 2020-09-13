f2d1fd2e2014d809b6b9a619556bfd7fdd12edf1
caballeto <elminsteraumar4@gmail.com>
2020-09-13 19:09:29 +0300

    Viginere encoding.

--- src/com/mokrousov/security/vigenere/Main.java
+++ src/com/mokrousov/security/vigenere/Main.java
@@ -1,7 +1,41 @@
 package com.mokrousov.security.vigenere;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
-  public static void main(String[] args) {
+  public static String encode(String text, String key) {
+    int keyLen = key.length();
+    key = preprocess(key);
+    text = text.toLowerCase();
+    StringBuilder sb = new StringBuilder(text.length());
+    int j = 0;
+    for (int i = 0; i < text.length(); i++) {
+      if (!Character.isLetter(text.charAt(i))) {
+        sb.append(text.charAt(i));
+        continue;
+      }
+
+      int c = text.charAt(i) - 'a';
+      int offset = key.charAt((j++) % keyLen) - 'a';
+      sb.append((char) ((c + offset) % 26 + 'a'));
+    }
+    
+    return sb.toString();
+  }
   
+  public static String preprocess(String text) {
+    return text.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+  }
+  
+  // TODO: uppercase letters
+  public static void main(String[] args) throws IOException {
+    // read text from file
+    // find the length of the key
+    // use frequency analysis to determine the mapping for each letter
+    //String text = Files.readString(Path.of("resources/encoded.txt"));
+    //System.out.println(text);
+    System.out.println(encode("The quick brown fox jumps over 13 lazy dogs.", "abc"));
   }
 }
